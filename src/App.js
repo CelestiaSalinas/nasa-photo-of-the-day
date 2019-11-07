6e610e5a1dcd35f8974d9a4e8c88e59acf8ca5ae
Celestia Salinas <csalinas3671@gmail.com>
2019-11-07 17:58:43 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
 import ImgList from "./components/DisplayComponents/ImgList";
-import Header from "./components/DisplayComponents/Header";
-
 function App() {
   return (
     <div className="App">
       <p>
-        <Header />
         <ImgList />
       </p>
     </div>
